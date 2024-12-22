cd1bb8ca3008a9babc499c7ecf976b49354ca920
Sokol995 <124513837+Sokol995@users.noreply.github.com>
2024-12-23 01:29:34 +0300

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,25 @@
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print('1st programm');
+    a = 9;
+    b = 5;
+    cont = a ** (0.5) * b;
+    print(cont);
+    print('2st programm');
+    a = 9.99;
+    b = 9.98;
+    c = 1000;
+    d = 1000.1;
+    bol = a > b and c != d;
+    print(bol)# Press Ctrl+F8 to toggle the breakpoint.
+    print('3nd programm');
+    a = 2 * 2 + 2;
+    b = 2 * (2 + 2);
+    print(a == b);
+    print('4nd programm');
+    a = '123.456';
+    b = float(a);
+    c = b * 10;
+    d = int(c);
+    e = d % 10;
+    print(e);
