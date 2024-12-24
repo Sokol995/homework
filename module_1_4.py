6f67f942e4cc604299e11a96ffcb52cd422ef82a
Sokol995 <124513837+Sokol995@users.noreply.github.com>
2024-12-24 22:37:14 +0300

    Add files via upload
    home_work_2

---
+++ module_1_4.py
@@ -0,0 +1,13 @@
+def print_hi(name):
+   my_string = input('')
+   result = my_string[0:-1:1]
+   print(result.count(''))
+   print(my_string.upper())
+   print(my_string.lower())
+   print(my_string.replace( ' ', ''))
+   print(my_string[0])
+   print(my_string[-1])
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
